2e3f0d731e92feb731e82dc4d1d76cfff34737c8
Alaminos <aalaminosz@gmail.com>
2018-08-22 18:22:39 +0200

    create new oscillators dynamically

--- test.js
+++ test.js
@@ -1,16 +1,37 @@
-const ctxAudio = new (window.AudioContext || window.webkitAudioContext)()
+const context = new (window.AudioContext || window.webkitAudioContext)()
 
-const oscillator = ctxAudio.createOscillator();
+const oscillator = context.createOscillator();
 oscillator.type = 'triangle'; //you can change to type square, sine, etc.
 oscillator.frequency = 180;
 oscillator.start();
 
-const oscillator220 = ctxAudio.createOscillator();
+const oscillator220 = context.createOscillator();
 oscillator220.type = 'square';
 oscillator220.frequency = 220;
 oscillator220.start();
 
+const clickableArea = document.getElementById('buttons');
+
+let osci_connect = false;
+let osci220_connect = false;
+
+clickableArea.addEventListener('click', 
+    (event) => {
+        let elemClicked = event.target;
+        if (elemClicked.id === 'btn_1') {
+            if (osci_connect) oscillator.disconnect(context.destination)
+            else oscillator.connect(context.destination);
+            osci_connect = !osci_connect;
+        } else if (elemClicked.id === 'btn_2') {
+            if (osci220_connect) oscillator220.disconnect(context.destination)
+            else oscillator220.connect(context.destination);
+            osci220_connect = !osci220_connect;
+        }
+        
+});
+
 /**
+ * All the above is working fine, but 
  * In order to build several oscillators dynamically:
  * Oscillator Class
 
@@ -22,27 +43,32 @@ const oscillators = [a, b, c];
      this.frequency = frequency;
  }
 
- Oscillator.prototype.newName()
 
-  * 
- */
+* 
+ **/
 
-const clickableArea = document.getElementById('buttons');
+const oscillators = []; //a list of potential oscillators
 
-let osci_connect = false;
-let osci220_connect = false;
+/*user indicates type and frequency and clicks on start oscillator,
+each form belong to an oscillator, so at the end there are three arguments passed:
+oscillator container (a,b,c), type, and frequency.
+Let's wrap it all into a func */
 
-clickableArea.addEventListener('click', 
-    (event) => {
-        let elemClicked = event.target;
-        if (elemClicked.id === 'btn_1') {
-            if (osci_connect) oscillator.disconnect(ctxAudio.destination)
-            else oscillator.connect(ctxAudio.destination);
-            osci_connect = !osci_connect;
-        } else if (elemClicked.id === 'btn_2') {
-            if (osci220_connect) oscillator220.disconnect(ctxAudio.destination)
-            else oscillator220.connect(ctxAudio.destination);
-            osci220_connect = !osci220_connect;
+const Oscillator = function(n, type, frequency) {
+    oscillators[n] = context.createOscillator();
+    oscillators[n].type = type;
+    oscillators[n].frequency = frequency;
+    oscillators[n].start();
+    oscillators[n].isPlaying = false; //in this property we will store if the oscillator is currently playing
+}
+
+
+ //when user clicks on Play/Stop btn
+ const play = function(n) {
+     if (isPlaying) {
+         oscillators[n].disconnect(context.destination);
+     } else {
+         oscillators[n].connect(context.destination);
         }
-        
-});
+    isPlaying = !isPlaying;
+ }
